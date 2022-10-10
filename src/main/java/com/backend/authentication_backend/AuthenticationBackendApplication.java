a37d04dccf71d25f689e8a7ec536ca1a63d03692
Zein <zeinkasar123@gmail.com>
2022-10-10 22:22:47 +0200

    Repo maken van het authentication_backen op github

---
+++ src/main/java/com/backend/authentication_backend/AuthenticationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.authentication_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationBackendApplication.class, args);
+	}
+
+}
